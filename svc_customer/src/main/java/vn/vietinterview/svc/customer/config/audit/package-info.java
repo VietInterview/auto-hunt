cf982f6831d17cc928748279fe592261624b5028
hienpv89 <hienpv89@gmail.com>
2018-10-02 09:47:24 +0700

    svc_customer

---
+++ svc_customer/src/main/java/vn/vietinterview/svc/customer/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package vn.vietinterview.svc.customer.config.audit;
---
+++ svc_customer/src/main/java/vn/vietinterview/svc/customer/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package vn.vietinterview.svc.customer.config;
